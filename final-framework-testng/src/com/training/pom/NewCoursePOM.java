f52f8ac89da942e2691e02f12bd71f7dcb5b0190
swaps2021 <46434784+swaps2021@users.noreply.github.com>
2019-01-14 19:00:36 +0530

    Update NewCoursePOM.java
    Added Comments for the methods

--- final-framework-testng/src/com/training/pom/NewCoursePOM.java
+++ final-framework-testng/src/com/training/pom/NewCoursePOM.java
@@ -41,31 +41,32 @@ private WebDriver driver;
 	}
 	
 	
-	
+	//This method will create a new course with the 3 parameters passes: title, category and code
 	public void createNewCourse(String title,String category, String code)
 	{
 		driver.findElement(By.linkText("Create a course")).click();
-		//driver.findElement(By.xpath("/html/body/div[1]/section/div/div[2]/div[1]/div/div[3]/div/div[2]/div/ul/li[1]/a")).click();
 		driver.findElement(By.id("title")).sendKeys(title);
 		driver.findElement(By.id("advanced_params")).click();
+		//Below webelement captures the 3rd option from dropdown
 		WebElement categ = driver.findElement(By.xpath("/html/body/div[1]/section/div/div[2]/div/section[2]/form/fieldset/div[3]/div[1]/div[1]/div/button"));
 		categ.click();
 		
 		driver.findElement(By.id("add_course_wanted_code")).sendKeys(code);
-		driver.findElement(By.id("add_course_submit")).click();
+		driver.findElement(By.id("add_course_submit")).click(); //Submit for creating new course
 	}
 	
+	//This method will enter course into details
 	public void courseIntro(String text) throws InterruptedException
 	{
 		driver.findElement(By.xpath("/html/body/div[1]/section/div/div[2]/div/section/div/div[1]/div/div[1]/a/em")).click();
 		Thread.sleep(5000);
-		driver.switchTo().frame(0);
+		driver.switchTo().frame(0);// Switching to intro editor frame
 		WebElement tEditor = driver.switchTo().activeElement();
 		new Actions(driver).moveToElement(tEditor).perform();
 		driver.findElement(By.xpath("/html/body")).sendKeys(text);
-		driver.switchTo().window(driver.getWindowHandle());
+		driver.switchTo().window(driver.getWindowHandle());// exiting from editor frame to page
 		Thread.sleep(3000);
-		driver.findElement(By.id("introduction_text_intro_cmdUpdate")).click();
+		driver.findElement(By.id("introduction_text_intro_cmdUpdate")).click();//Updating the Course Intro
 		}
 
 }
